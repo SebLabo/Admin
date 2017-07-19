9d44e63145b913fbebbf41c2ce17b26b4d744cd9
Sébastien Fouvet <sfouvet@webqam.fr>
2017-07-19 18:01:16 +0200

    branche dev parallele

---
+++ ADMIN/CONTROL/Abstract.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: webqa
+ * Date: 19/07/2017
+ * Time: 17:17
+ */
+Abstract class Admin_Abstract extends Core_Abstract
+{
+    private $btnAdd = true;
+    private $btnEdit = true;
+    private $btnDelete = true;
+    private $diplayCols = array();
+    private $itemsPerPage = 30;
+    private $othersCols = array();
+    private $message = null;
+
+
+    public function setAddBtn(bool $value)
+    {
+        $this->btnAdd = $value;
+    }
+
+    public function setEditBtn(bool $value)
+    {
+        $this->btnEdit = $value;
+    }
+
+    public function setDeleteBtn(bool $value)
+    {
+        $this->btnDelete = $value;
+    }
+
+    public function setItemNumber(int $value)
+    {
+        $this->itemsPerPage = $value;
+    }
+
+    public function setDisplayCols(array $tab)
+    {
+        $this->diplayCols = $tab;
+    }
+
+    public function setOthersCols(array $tab)
+    {
+        $this->othersCols = $tab;
+    }
+
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public function Initialise()
+    {
+
+    }
+}
+
---
+++ FRONT/CONTROL/Abstract.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: webqa
+ * Date: 19/07/2017
+ * Time: 17:17
+ */
+Abstract class Front_Abstract extends Core_Abstract
+{
+
+}
