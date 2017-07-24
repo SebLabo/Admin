81f0a7b95cecc616357152790e3b800cadcd4ffb
Seb <sebastien.fouvet@free.fr>
2017-07-24 08:12:01 +0200

    travail dispatch

--- Master.php
+++ Master.php
@@ -6,9 +6,37 @@
  * Time: 17:13
  */
 
-include "Library/My/divers.php";
 
-//$urldetails=getURLinfos();
-//Aff($urldetails);
+class Master_Dispatch
+{
+    private $module;
+    private $page;
+    private $action;
+    private $get = array();
+    private $page404 = false;
 
-aff($_SERVER);
+    public function __construct($Url)
+    {
+
+        $dispatch = explode('/', $url);
+        $NbArguments = count($dispatch);
+        if ($taille < 3) {
+            $this->page404 = true;
+        }
+        $this->module = $chemin[1];
+        $this->page = $chemin[2];
+        if ($taille > 3) {
+            $this->action = $chemin[3];
+        }
+        if ($taille > 4) {
+            for ($i = 4; $i < $taille; $i++) {
+                $arg = explode('_', $chemin[$i]);
+                $this->get[$arg[0]] = $arg[1];
+            }
+        }
+
+        Aff($this);
+
+
+    }
+}
---
+++ Pages/Core/CONTROL/Page404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SEB
+ * Date: 22/07/2017
+ * Time: 16:19
+ */
+
+class Core_Page404 extends Core_Abstract
+{
+
+}
