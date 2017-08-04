b437ebb3158e76ddcd00953246ea68e981192975
Sébastien Fouvet <sfouvet@webqam.fr>
2017-08-04 13:38:19 +0200

    new files

--- Pages/Core/CONTROL/Abstract.php
+++ Pages/Core/CONTROL/Abstract.php
@@ -6,7 +6,18 @@
  * Date: 19/07/2017
  * Time: 17:18
  */
-class Core_Abstract
+abstract class Core_Abstract
 {
+
+
+    private function redirect($path)
+    {
+
+    }
+
+    private function translate()
+    {
+
+    }
 
 }
---
+++ Pages/Core/View/Form/Core_Form_Abstract.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Seb
+ * Date: 04/08/2017
+ * Time: 13:35
+ */
+
+abstract class Core_Form_Abstract
+{
+
+
+}
