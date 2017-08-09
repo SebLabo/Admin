3c2f63da92349349cad226acc4f979cc96ad55cd
Seb <sebastien.fouvet@free.fr>
2017-08-09 22:53:03 +0200

    JS +CSS

---
+++ Pages/Front/CONTROL/Abstract.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: seb
+ * Date: 19/07/2017
+ * Time: 17:17
+ */
+Abstract class Front_Abstract extends Core_Abstract
+{
+
+}
--- index.php
+++ index.php
@@ -5,7 +5,21 @@
  * Date: 21/07/2017
  * Time: 17:31
  */
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+<?php
 include "Library/My/divers.php";
 include "Master.php";
-
+?>
+<div id='matrix'>
+    <p>Seb</p>
+</div>
+<?php
 $master = new Master_Dispatch($_SERVER['REQUEST_URI']);
+?>
+</body>
+</html>
